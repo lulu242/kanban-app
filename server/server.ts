5c4ca20b2c2deed39cfd3701765e0eda4dd6f585
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-12-29 01:52:41 +0900

    init: db연결

--- server/server.ts
+++ server/server.ts
@@ -2,12 +2,17 @@ import express, { Request, Response } from "express";
 import boardRounter from "./routes/board.router";
 import listsRounter from "./routes/lists.router";
 import cardsRounter from "./routes/cards.router";
+import mongoose from "mongoose";
 
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.json())
+
+mongoose.connect('mongodb+srv://kanban:kanban3242@expressapp.btqyyca.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('db Connect'))
+  .catch(err => console.log(err))
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Welcome')
