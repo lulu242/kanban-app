2419dfc09d5228f198bf415849ae8f16afc24024
lulu242 <119933024+lulu242@users.noreply.github.com>
2024-01-18 22:45:36 +0900

    feat: login 시 card반환

--- server/controllers/user.controllers.ts
+++ server/controllers/user.controllers.ts
@@ -9,16 +9,24 @@ async function signUp(req: Request, res: Response, next: NextFunction) {
   } catch (error) {
     return next(error);
   }
-  res.status(201).json(createUsre);
+  res.status(201).send('회원가입 완료');
 }
 
 
 async function logIn(req: Request, res: Response, next: NextFunction) {
   try {
-    const user = await User.find({userId: req.body.userId, password: req.body.password}).populate('listsId')
-    if(user) {
-      res.status(200).json(user);
-    } else {res.status(400).json({error: 'User not found'})}
+    const user = await User.findOne({ userId: req.body.userId, password: req.body.password }, '-password').populate({
+      path: 'lists',
+      populate: {
+        path: 'cards',
+      },
+    })
+    // populate 중첩 사용하면 성능이 안 좋아 질 수 있음 getlists로 한 번더 요청 보내는 걸로 수정
+    if (!user) {
+      return res.status(400).json({ error: 'User not found' })
+    }
+
+    res.status(200).json(user);
   } catch (error) {
     next(error);
   }
