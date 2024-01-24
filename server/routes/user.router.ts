1e5bd4399bf5307387126e966be71671697762ab
lulu242 <119933024+lulu242@users.noreply.github.com>
2024-01-25 06:40:42 +0900

    feat: 회원가입 에러 메시지 수정

--- server/routes/user.router.ts
+++ server/routes/user.router.ts
@@ -10,13 +10,15 @@ UserRounter.get('/', logIn)
 UserRounter.post('/',
   [
     check('userId')
-      .isAlphanumeric()
       .isLength({ min: 5, max: 20 })
-      .withMessage('아이디는 5~20자의 영문 소문자와 숫자로만 사용 가능합니다.'),
+      .withMessage('아이디는 5~20자여야 합니다')
+      .isAlphanumeric()
+      .withMessage('아이디는 영문자와 숫자로만 사용 가능합니다.'),
     check('password')
-      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)
       .isLength({ min: 6, max: 16 })
-      .withMessage('비밀번호는 6~16자의 영문 대/소문자, 숫자, 특수문자를 사용해야 합니다.')
+      .withMessage('비밀번호는 6~16자여야 합니다.')
+      .matches(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]*$/)
+      .withMessage('비밀번호는 영문자, 숫자, 특수문자를 모두 사용해야 합니다.')
   ],
   signUp
 )
